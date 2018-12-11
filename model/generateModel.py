32b39cff47aaaa86791bdd0b09429e711aaf0e1d
Diego Sanz Alonso <diego070795@hotmail.com>
2018-12-11 19:44:49 +0100

    #28 Incluidas distintas métricas (precision, recall, f1-score, auc)

--- model/generateModel.py
+++ model/generateModel.py
@@ -5,6 +5,12 @@ from sklearn.model_selection import cross_val_score
 from sklearn.ensemble import RandomForestClassifier
 import pickle
 
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import f1_score
+from sklearn.metrics import roc_auc_score
+
 input_data = "../datasets/dataframes/"
 
 print("Reading data")
@@ -17,23 +23,33 @@ classes_df_bug = classes_df[classes_df['Number of bugs'] > 0]
 
 # Get same buggy and non-buggy files and shuffle it
 muestra = 1000
-classes_df_partial = classes_df_nobug.sample(muestra).append(classes_df_bug.sample(muestra)).sample(frac=1)
+classes_df = classes_df_nobug.sample(muestra).append(classes_df_bug.sample(muestra)).sample(frac=1)
 
-# Attributes
-classes_atts = classes_df_partial.drop("Number of bugs", axis = 1)
-# Class: Buggy vs. Non-buggy files. Number of bugs is unnecesary for the moment
-classes_label = classes_df_partial["Number of bugs"] > 0
+# Separate attributes and label
+classes_df_atts = classes_df.drop('Number of bugs', axis = 1)
+classes_df_label = classes_df['Number of bugs'] > 0
+
+# Train-Test split
+X_train, X_test, y_train, y_test = train_test_split( classes_df_atts, classes_df_label, test_size=0.33, random_state=3)
+
 
 print("Creating Random Forest Classifier")
 # Basic classificator
 clf = RandomForestClassifier(n_estimators=100, max_depth=2, random_state=0)
 
-print("Cross validation")
-# Cross-validation scores, for checking in model can predict something
-print(cross_val_score(clf, classes_atts, classes_label, cv = 5))
-
 # Train classifier
-clf.fit(classes_atts, classes_label)
+clf.fit(X_train, y_train)
+
+# Predict test set
+prediction = clf.predict(X_test)
+
+# Calculate different metrics
+print('Metrics:')
+print('Accuracy:', accuracy_score(y_test,prediction))
+print('Precision: ', precision_score(y_test,prediction))
+print('Recall: ', recall_score(y_test,prediction))
+print('F-1 Score: ', f1_score(y_test,prediction))
+print('Area Under Curve: ', roc_auc_score(y_test,prediction))
 
 # Save in a Python object, so it can be used in future without classifing again
 print("Saving classifier")
