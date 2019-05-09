3c3902970f76bfd2286bd041b45bce313a969594
PabloB <pablo.bermejo@uclm.es>
2019-05-09 14:04:41 +0200

    auth heroku registri con docker login y heroku api key

--- travis_deploy.sh
+++ travis_deploy.sh
@@ -5,10 +5,9 @@ export REPO=tfm-diego-heroku
 docker pull paberlo/alpine-scikit-django-jdk8:latest
 
 echo "login a heroku"
-#se ha usado el método 2 de:
-#https://stackoverflow.com/questions/39554697/script-heroku-login-in-a-ci-environment
-
-echo "$HEROKU_PASSWORD" | docker login -u "$HEROKU_USERNAME" --password-stdin registry.heroku.com
+#prueba 1:https://stackoverflow.com/questions/39554697/script-heroku-login-in-a-ci-environment
+#prueba 2: echo "$HEROKU_PASSWORD" | docker login -u "$HEROKU_USERNAME" --password-stdin registry.heroku.com
+docker login --username=_ --password=$HEROKU_API_KEY registry.heroku.com
 export TAG=web
 docker build -f $TRAVIS_BUILD_DIR/Dockerfile-heroku-web -t $REPO/$TAG .
 docker tag $REPO/$TAG:latest registry.heroku.com/tfm-diego/$TAG
