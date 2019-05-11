f0577bb87a83be3301c12849ba3ffc591f12e641
Pablo <pablo.bermejo@uclm.es>
2019-05-11 17:43:41 +0200

    los parametros estaban bien [ci skip]

--- webapp/main/views.py
+++ webapp/main/views.py
@@ -64,7 +64,7 @@ def index(request):
 			#archive.close()
 
 			# Mandar trabajo a la cola
-			job1 = q.enqueue(generate_repo_atts)
+			job1 = q.enqueue(generate_repo_atts, file.name[:-4])
 			time.sleep(1)  # dar tiempo para que job1 acabe rápidamente si ve que ya están los datos
 			# no se pueden usar watchdogs porque esto es front end, y el worker se baja las carpetas
 
@@ -122,7 +122,7 @@ def index(request):
 						</html>
 				"""
 				return HttpResponse(html)
-			job2 = q.enqueue(predict_buggy_files)
+			job2 = q.enqueue(predict_buggy_files, file.name[:-4])
 			while not job2.status == 'finished':  # es rápido
 				time.sleep(2)
 			return HttpResponse(job2.result)
